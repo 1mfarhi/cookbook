8786c57fff656e56199c152e02eccb7a8ff91ec8
1mfarhi <manuelafarhi@sbcglobal.net>
2022-10-13 00:27:54 -0500

    users

--- users/forms.py
+++ users/forms.py
@@ -1,27 +1,24 @@
-from profile import Profile
-import users.models
-from django import forms
-from django.forms import ModelForm
-from django.contrib.auth.models import User
-from django.core.validators import validate_email
-from django.contrib.auth.forms import UserCreationForm
-from .models import *
+# from profile import Profile
+# import users.models
+# from django.forms import ModelForm
+# from django.core.validators import validate_email
+# from .models import *
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+# class UserRegisterForm(UserCreationForm):
+#     email = forms.EmailField()
 
-    class Meta:
-        model = User
-        # fields = ['username', 'email', 'password1', 'password2']   
-        exclude = ['user']
+#     class Meta:
+#         model = User
+#         # fields = ['username', 'email', 'password1', 'password2']   
+#         exclude = ['user']
 
-class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
-    class Meta:
-        model = User
-        fields = ["username", "email"]
+# class UserUpdateForm(forms.ModelForm):
+#     email = forms.EmailField()
+#     class Meta:
+#         model = User
+#         fields = ["username", "email"]
 
-class ProfileUpdateForm(forms.ModelForm):
-    Profile = models.ForeignKey("users.Profile", on_delete=models.CASCADE, verbose_name="Report Author")
-    model = Profile
-    fields = ["image"]
+# class ProfileUpdateForm(forms.ModelForm):
+#     Profile = models.ForeignKey("users.Profile", on_delete=models.CASCADE, verbose_name="Report Author")
+#     model = Profile
+#     fields = ["image"]
